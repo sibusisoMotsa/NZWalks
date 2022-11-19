37f7efcd708693323c5ee557094dc634f00b3ff9
Sibusiso <sibusisomotsa373@gmail.com>
2022-11-19 18:09:34 +0200

    Section 2 - Async API

---
+++ NZWalks/NZWalks.API/Models/DTO/Region.cs
@@ -0,0 +1,13 @@
+﻿namespace NZWalks.API.Models.DTO
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Area { get; set; }
+        public double Lat { get; set; }
+        public double Log { get; set; }
+        public long Population { get; set; }
+    }
+}
